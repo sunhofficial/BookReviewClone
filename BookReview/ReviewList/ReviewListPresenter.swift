290195d42113100c29efd4c312aa5a3f42994a5a
Sunho <sunh803@naver.com>
2023-05-28 11:19:40 +0900

    feat: Review List Viewcontroller add

--- BookReview/ReviewList/ReviewListPresenter.swift
+++ BookReview/ReviewList/ReviewListPresenter.swift
@@ -5,11 +5,32 @@
 //  Created by 235 on 2023/05/23.
 //
 
-import Foundation
-protocol ReviewListProtocol{}
-final class ReviewListPresenter{
+import UIKit
+protocol ReviewListProtocol{
+    func setupNavigationBar()
+    func setupVIews()
+}
+final class ReviewListPresenter : NSObject{
     private let viewController : ReviewListProtocol
     init(viewController: ReviewListProtocol) {
         self.viewController = viewController
     }
+    func viewDidLoad(){
+        viewController.setupNavigationBar()
+        viewController.setupVIews()
+    }
 }
+extension ReviewListPresenter: UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        3
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: nil)
+        var content = cell.defaultContentConfiguration()
+        content.text = "\(indexPath.row)"
+        cell.contentConfiguration = content
+        
+        return cell
+    }
+}
+
--- BookReview/ReviewList/ReviewListViewController.swift
+++ BookReview/ReviewList/ReviewListViewController.swift
@@ -6,15 +6,36 @@
 //
 
 import UIKit
-
+import SnapKit
 class ReviewListViewController: UIViewController {
     private lazy var presenter = ReviewListPresenter(viewController: self)
+    private lazy var tableView : UITableView = {
+        let tableView  = UITableView()
+        tableView.dataSource = presenter
+        return tableView
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
+        presenter.viewDidLoad()
+        
         // Do any additional setup after loading the view.
     }   
 
 
 }
 
-extension ReviewListViewController : ReviewListProtocol {}
+extension ReviewListViewController : ReviewListProtocol {
+    func setupNavigationBar(){
+        navigationItem.title = "도서 리뷰"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        let rightBarButtonItem = UIBarButtonItem(
+            barButtonSystemItem: .add, target: self, action: nil)
+        navigationItem.rightBarButtonItem = rightBarButtonItem
+    }
+    func setupVIews(){
+        view.addSubview(tableView)
+        tableView.snp.makeConstraints{
+            $0.edges.equalToSuperview()
+        }
+    }
+}
